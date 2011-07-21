4dcecacb2b241b32cc9d1fd9955ae9079f79fcd8
Fabio Miranda Costa <fabiomcosta@gmail.com>
2011-07-21 00:54:21 -0300

    a great rewrite of the array module, looks beta!

--- lib/array.c
+++ lib/array.c
@@ -6,11 +6,12 @@
 #include "assert.h"
 
 
-array*
+void
 array_new(array* arr) {
 
-    if (!arr){
-        arr = malloc(sizeof(*arr));
+    if (arr->itens == NULL){
+
+        arr->itens = malloc(sizeof(*arr->itens));
 
         if (!arr){
             ERROR("ERROR: Couldn't allocate memory.\n");
@@ -19,9 +20,11 @@ array_new(array* arr) {
 
         arr->length = 0;
         arr->_n_allocated = 0;
-    }
 
+    }
+    
     if (arr->length == arr->_n_allocated) {
+
         int alloc;
 
         if (arr->_n_allocated == 0) {
@@ -42,17 +45,14 @@ array_new(array* arr) {
         arr->_n_allocated = alloc;
     }
 
-    return arr;
 }
 
-
-array*
-array_push(array *arr, void *item, char const should_free) {
-    arr = array_new(arr);
+void
+array_push(array *arr, void *item, char const is_copy) {
+    array_new(arr);
     arr->itens[arr->length].item = item;
-    arr->itens[arr->length].should_free = should_free;
+    arr->itens[arr->length]._is_copy = is_copy;
     arr->length++;
-    return arr;
 }
 
 
@@ -60,17 +60,16 @@ void
 array_free(array *arr) {
     int i;
     for (i = 0; i < arr->length; i++) {
-        if (arr->itens[i].should_free){
+        if (arr->itens[i]._is_copy){
             free(arr->itens[i].item);
         }
     }
     free(arr->itens);
-    free(arr);
 }
 
 
 void*
-array_copy_value(void *value, int len) {
+copy(void *value, int len) {
    void *ret = malloc(len);
    if (!ret) {
       ERROR("ERROR: value duplication failed.\n");
@@ -82,38 +81,51 @@ array_copy_value(void *value, int len) {
 
 
 int main(){
-    ARRAY_NEW(a);
+    array a;
+    a.itens = NULL;
+    array_new(&a);
 
-    assert_equals_int(a->length, 0);
-    assert_equals_int(a->_n_allocated, 4);
+    assert_equals_int(a.length, 0);
+    assert_equals_int(a._n_allocated, 4);
 
     int i = 10;
-    ARRAY_PUSH(a, i);
+    ARRAY_PUSH(&a, i);
 
-    assert_equals_int(a->length, 1);
-    assert_equals_int(a->_n_allocated, 4);
+    assert_equals_int(a.length, 1);
+    assert_equals_int(a._n_allocated, 4);
 
     assert_equals_int(ARRAY_GET(a, 0, int), 10);
+
+    int j = 12, h = 13, k = 14;
+    ARRAY_PUSH(&a, j);
+    ARRAY_PUSH(&a, h);
+    ARRAY_PUSH(&a, k);
+
+    // iterating
+    int _i;
+    for (_i = 0; _i < a.length; _i++) {
+        printf("a[%d]: %d\n", _i, ARRAY_GET(a, _i, int));
+    }
 
     char b = 'c';
     char b2 = 'c';
     char b3 = 'c';
 
-    ARRAY_PUSH(a, b);
-    ARRAY_PUSH(a, b2);
-    ARRAY_PUSH(a, b3);
+    ARRAY_PUSH(&a, b);
+    ARRAY_PUSH(&a, b2);
+    ARRAY_PUSH(&a, b3);
 
-    assert(ARRAY_GET(a, 1, char) == 'c');
-    assert(ARRAY_GET(a, 2, char) == 'c');
-    assert(ARRAY_GET(a, 3, char) == 'c');
+    assert(ARRAY_GET(a, 4, char) == 'c');
+    assert(ARRAY_GET(a, 5, char) == 'c');
+    assert(ARRAY_GET(a, 6, char) == 'c');
 
     char c[] = "a long phrase with some chars\n";
 
-    ARRAY_PUSH_PTR(a, c);
-    printf(ARRAY_GET_PTR(a, 4, char*));
-    assert(strcmp(ARRAY_GET_PTR(a, 4, char*), "a long phrase with some chars\n") == 0);
+    ARRAY_PUSH_PTR(&a, c);
+    printf("%s", ARRAY_GET_PTR(a, 7, char*));
+    assert(strcmp(ARRAY_GET_PTR(a, 7, char*), "a long phrase with some chars\n") == 0);
 
-    array_free(a);
+    array_free(&a);
 
     return 0;
 }
--- lib/array.h
+++ lib/array.h
@@ -1,4 +1,9 @@
 // based on http://lelanthran.com/deranged/?p=68
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "print.h"
+#include "assert.h"
 
 #ifndef __ARRAY__
 #define __ARRAY__
@@ -7,19 +12,15 @@
 #define ARRAY_GROWTH_RATE 2
 #define ARRAY_DEFAULT_BLOCK_SIZE 4
 
-#define ARRAY_NEW(_array)\
-    array *_array = NULL;\
-    _array = array_new(_array);
+#define ARRAY_GET(_array, index, _type) *((_type*)(_array.itens[index].item))
+#define ARRAY_GET_PTR(_array, index, _type) (_type)(_array.itens[index].item)
 
-#define ARRAY_GET(_array, index, _type) *((_type *)(_array->itens[index].item))
-#define ARRAY_GET_PTR(_array, index, _type) (_type)(_array->itens[index].item)
-
-#define ARRAY_PUSH(_array, val) _array = array_push(_array, array_copy_value(&val, sizeof(val)), 1);
-#define ARRAY_PUSH_PTR(_array, val) _array = array_push(_array, val, 0);
+#define ARRAY_PUSH(_array, val) array_push(_array, copy(&val, sizeof(val)), 1);
+#define ARRAY_PUSH_PTR(_array, val) array_push(_array, val, 0);
 
 typedef struct {
     void *item;
-    char should_free;
+    char _is_copy;
 } array_item;
 
 typedef struct {
@@ -28,12 +29,10 @@ typedef struct {
     int _n_allocated; //n of elements alocated in memory
 } array;
 
-array* array_new(array*);
-//array_item array_new_item(char*, int);
-array* array_push(array*, void*, char const);
-//int array_contains(array*, char*);
+void array_new(array*);
+void array_push(array*, void*, char const);
 int array_get(array*, char*);
-void* array_copy_value(void*, int);
+void* copy(void*, int);
 void array_free(array*);
 
 #endif
